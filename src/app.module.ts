887128adf0e398e3c7d43442ac6644942daea46f
dumitrupascal <dimapascal@gmail.com>
2020-04-25 21:52:27 +0300

    Add pg_admin container, update env file, update app module to use env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-
+import { join } from 'path'
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'postgres',
-      port: 5433,
-      username: 'admin',
-      password: 'admin',
-      database: 'testdb',
-      entities: [],
+      host:  process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [join(__dirname, './**/*.entity{.ts,.js}')],
       synchronize: true,
-      logging: true,
+      logging: 'all',
     }),
     ConfigModule.forRoot({
       isGlobal: true,
